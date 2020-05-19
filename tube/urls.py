a384420d40d0e81c430cf5f5030b8427f5dab9c3
YopsiQ <yopsiq@gmail.com>
2020-05-20 02:19:10 +0300

    carriage return

--- tube/urls.py
+++ tube/urls.py
@@ -5,6 +5,5 @@ from tube.views import VideoView, VideoDetailView
 
 urlpatterns = [
     path('upload/', VideoView.as_view(), name='upload_video'),
-    path('uploaded/<int:pk>/', VideoDetailView.as_view(),
-         name='video_detail'),
+    path('uploaded/<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
 ]
